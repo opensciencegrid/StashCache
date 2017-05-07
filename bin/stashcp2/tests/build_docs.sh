997fb4426c505ad31fc0274e8608f91c9c7f60be
Derek Weitzel <djw8605@gmail.com>
2017-05-07 14:04:47 -0500

    Fix naming of deploy key

--- bin/stashcp2/tests/build_docs.sh
+++ bin/stashcp2/tests/build_docs.sh
@@ -3,9 +3,9 @@
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
 
   openssl aes-256-cbc -K $encrypted_e92dfea3fdc7_key -iv $encrypted_e92dfea3fdc7_iv -in deploy-key.enc -out deploy-key -d
-  chmod 600 deploy_key
+  chmod 600 deploy-key
   eval `ssh-agent -s`
-  ssh-add deploy_key
+  ssh-add deploy-key
   git config user.name "Automatic Publish"
   git config user.email "djw8605@gmail.com"
   git remote add gh-token "git@github.com:opensciencegrid/StashCache.git"
